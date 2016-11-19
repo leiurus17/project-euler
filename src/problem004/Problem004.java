e8c76a88455fdc35222f17525a93679c20e97b74
Marlon Monzon <moonrock650@gmail.com>
2016-11-19 23:46:02 +0800

    Code for Problem 004

--- src/problem004/Problem004.java
+++ src/problem004/Problem004.java
@@ -4,4 +4,33 @@ package problem004;
 
 public class Problem004 {
 
+	public static void main(String[] args) {
+		long loopAnswer;
+		long largerPalindrome = 0;
+
+		for (int x = 100; x <= 999; x++) {
+			for (int y = 100; y <= 999; y++) {
+				loopAnswer = processAnswer(x * y);
+
+				if (loopAnswer > largerPalindrome) {
+					largerPalindrome = loopAnswer;
+					System.out.println(largerPalindrome);
+				}
+
+			}
+		}
+
+	}
+
+	public static long processAnswer(long loopAnswer) {
+		String answerInStringProper = Long.toString(loopAnswer);
+		String answerInReverse = new StringBuffer(answerInStringProper).reverse().toString();
+
+		if (answerInStringProper.equalsIgnoreCase(answerInReverse)) { // Palindrome
+			return loopAnswer;
+		}
+
+		return 0;
+	}
+
 }
