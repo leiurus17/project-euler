1a50b91b1b4da6d443cf468695c81d5d3b31e321
Marlon Monzon <moonrock650@gmail.com>
2016-11-26 15:31:00 +0800

    Failed attempt at answering Problem007

---
+++ src/problem007/Problem007.java
@@ -0,0 +1,54 @@
+package problem007;
+
+/**
+ * 
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+ * that the 6th prime is 13.
+ * 
+ * What is the 10 001st prime number?
+ * 
+ * @author martianworm17
+ *
+ */
+
+public class Problem007 {
+
+	public static void main(String[] args) {
+		System.out.println("I still don't understand prime numbers.");
+	}
+
+	// public static void main(String[] args) {
+	//
+	// int primeNumberCounter = 168;
+	// int counter = 0;
+	// int prime = 0; // One is not a prime number .
+	//
+	// do {
+	//
+	// if (prime == 2 || prime == 3) {
+	// counter++;
+	// } else if (prime == 1) {
+	// // Do nothing. Not a prime number.
+	// }
+	//
+	// else if (prime % 2 != 0 && prime % 3 != 0 && Math.sqrt(prime) % 2 != 0) {
+	// System.out.println(prime + " is a prime number.");
+	// counter++;
+	// // System.out.println("Prime count is now: " + counter);
+	// } else {
+	// // System.out.println(prime + " is not a prime number.");
+	// }
+	//
+	// if (counter == primeNumberCounter) {
+	// break;
+	// }
+	//
+	// prime++; // Increment prime after checking
+	//
+	// } while (counter <= primeNumberCounter);
+	//
+	// System.out.println("10,001st prime number is: " + prime);
+	//
+	// }
+
+}
